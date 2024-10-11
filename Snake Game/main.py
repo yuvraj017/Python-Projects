8c3ffc770ebf8576621cec4daa86c2d0a2a0f5b0
CptAsura <y.s.bhati.017@gmail.com>
2024-10-11 16:42:21 +0530

    Try

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -15,10 +15,10 @@ food = Food()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up, "up")
-screen.onkey(snake.down, "down")
-screen.onkey(snake.left, "left")
-screen.onkey(snake.right, "right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
 game_is_on = True
